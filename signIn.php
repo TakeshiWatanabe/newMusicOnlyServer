d3aac9f0714ad9c698d9fad71730256ae6622f33
渡邉　剛志 <takeshi.w9@icloud.com>
2015-05-31 19:53:44 +0800

    ok

--- signIn.php
+++ signIn.php
@@ -4,13 +4,18 @@
 $db = mysqli_connect('localhost', 'root', 'camp2015', 'iosMusic') or die(mysqli_connect_error());
 mysqli_set_charset($db, 'utf8');
 
-
 //データの取得
 $sql = 'select * from signIn';
 $recordset = mysqli_query($db, $sql) or die(mysqli_error($db));
 $allEventData = array();
 while ($eventData = mysqli_fetch_assoc($recordset)){
 
+	//連想配列($array)
+	$recordset = array(
+		"userName" => "たくや",
+		"password" => ""
+	),
+);
 $allEventData[] = $eventData; 	
 }
 
